79c92ae83475009e8cd0ae92d9b6fddf61e4fc3a
root <root@localhost.localdomain>
2018-04-06 09:02:32 +0800

    delete file

--- prjNsLogistics.part1_/prjNsLogistics/src/com/bu/mars/ibusiness.java
+++
@@ -1,9 +0,0 @@
-package com.bu.mars;
-import java.util.*;
-public interface ibusiness {
-	public boolean save(Object ob);
-	public boolean update(Object ob);
-	public boolean delete(String id);
-	public Object findById(String id);
-	public Vector findAll();
-}
